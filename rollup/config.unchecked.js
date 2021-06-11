46773c0ae7f217de13762f96a7273f5cf74437a1
axmand <815966508@qq.com>
2021-06-12 00:18:32 +0800

    fx

--- rollup/config.unchecked.js
+++
@@ -1,8 +0,0 @@
-var config = require('./config')
-var removeCheck = require('./plugins/remove-check')
-
-config.dest = 'dist/regl.unchecked.js'
-config.sourceMap = false
-config.plugins.push(removeCheck())
-
-module.exports = config
